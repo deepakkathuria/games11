943f24c3de3001755ba3cdfdce7288db306f66f3
divyakathuriya <deepakkathuria32@gmail.com>
2024-08-03 14:43:04 +0530

    Player fix

--- app.js
+++ app.js
@@ -1768,6 +1768,41 @@ app.get("/top-players/venue/:venueId/teams/:teamId1/:teamId2", async (req, res) 
 });
 
 
+// app.get("/frequent-leaders/venue/:venueId/teams/:teamId1/:teamId2", async (req, res) => {
+//   const { venueId, teamId1, teamId2 } = req.params;
+
+//   try {
+//     const query = `
+//       SELECT 
+//         dt.player_id,
+//         p.first_name,
+//         p.last_name,
+//         p.playing_role,
+//         t.name AS team_name,
+//         t.short_name AS team_short_name, 
+//         SUM(CASE WHEN dt.role = 'Captain' THEN 1 ELSE 0 END) AS times_captain,
+//         SUM(CASE WHEN dt.role = 'Vice Captain' THEN 1 ELSE 0 END) AS times_vice_captain,
+//         GROUP_CONCAT(DISTINCT dt.match_id ORDER BY dt.match_id) AS match_ids,
+//         COUNT(DISTINCT dt.match_id) AS match_count
+//       FROM DreamTeam_test dt
+//       JOIN matches m ON dt.match_id = m.id
+//       JOIN players p ON dt.player_id = p.id
+//       JOIN team_players tp ON dt.player_id = tp.player_id
+//       JOIN teams t ON tp.team_id = t.id
+//       WHERE (m.team_1 = ? OR m.team_2 = ?) AND (m.team_1 = ? OR m.team_2 = ?)
+//         AND m.venue_id = ?
+//       GROUP BY dt.player_id, p.playing_role, t.name, t.short_name
+//       ORDER BY times_captain DESC, times_vice_captain DESC;
+//     `;
+
+//     const [results] = await pool.query(query, [teamId1, teamId2, teamId1, teamId2, venueId]);
+//     res.json(results);
+//   } catch (error) {
+//     console.error("Error fetching frequent captains and vice captains:", error);
+//     res.status(500).send("Failed to retrieve data");
+//   }
+// });
+
 app.get("/frequent-leaders/venue/:venueId/teams/:teamId1/:teamId2", async (req, res) => {
   const { venueId, teamId1, teamId2 } = req.params;
 
@@ -1791,17 +1826,23 @@ app.get("/frequent-leaders/venue/:venueId/teams/:teamId1/:teamId2", async (req, 
       JOIN teams t ON tp.team_id = t.id
       WHERE (m.team_1 = ? OR m.team_2 = ?) AND (m.team_1 = ? OR m.team_2 = ?)
         AND m.venue_id = ?
+        AND (t.id = ? OR t.id = ?)
       GROUP BY dt.player_id, p.playing_role, t.name, t.short_name
       ORDER BY times_captain DESC, times_vice_captain DESC;
     `;
 
-    const [results] = await pool.query(query, [teamId1, teamId2, teamId1, teamId2, venueId]);
+    const [results] = await pool.query(query, [teamId1, teamId2, teamId1, teamId2, venueId, teamId1, teamId2]);
     res.json(results);
   } catch (error) {
     console.error("Error fetching frequent captains and vice captains:", error);
     res.status(500).send("Failed to retrieve data");
   }
 });
+
+
+
+
+
 
 
 app.get("/match-stats/:venueId/:teamId", async (req, res) => {
